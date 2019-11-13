e0996a3be22eed42f3cd9e2d8c1899b769a7cb3c
Kaitlyn Giovinazzo <kgiovinazzo42@gmail.com>
2019-11-12 22:04:54 -0500

    Adding SQL file

---
+++ bamazonSchema.sh
@@ -0,0 +1,13 @@
+-- CREATE DATABASE bamazon;
+USE bamazon;
+-- DROP TABLE products;
+-- CREATE TABLE products (
+-- item_id INTEGER(11) NOT NULL AUTO_INCREMENT,
+-- product_name VARCHAR(100) NOT NULL,
+-- department_name VARCHAR(100) NOT NULL,
+-- price DECIMAL (10, 2),
+-- stock_quantity INTEGER default (0),
+-- PRIMARY KEY (item_id)
+-- );
+
+SELECT * FROM products;
